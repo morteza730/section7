940e546e7cf38df04b1c36773609c656709377ba
morteza <student13730@gmail.com>
2021-05-09 23:15:19 +0430

    commit project files

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import re
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -8,10 +9,11 @@ from resourses.items import Item, ItemList
 from resourses.stores import Stores, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://sqwjfwhvaczgeu" \
-                                        ":5741a1980d0229d3d1c311bdaa42eab6920409e6c9a391e78ad0f3f5fcb16871@ec2-54-164" \
-                                        "-22-242.compute-1.amazonaws.com:5432/d396up6h1c6v6u"#os.environ.get(
-# 'DATABASE_URL')#, 'sqlite:///data.db'
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
+# 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "sadeghi"
 jwt = JWT(app, authentication, identify)  # /auth
